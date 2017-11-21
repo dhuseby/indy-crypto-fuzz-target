f80ed97e30d37c8985b64ff8d8909289f51bd9c9
Dave Huseby <dhuseby@linuxfoundation.org>
2017-11-21 11:21:45 -0800

    fuzzing SignKey::new works

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ extern crate indy_crypto;
 
 fn main() {
     afl::read_stdio_bytes(|bytes| {
-        indy_crypto::Signkey::new(Some(&bytes)).unwrap();
+        indy_crypto::bls::SignKey::new(Some(&bytes)).unwrap();
     });
 }
